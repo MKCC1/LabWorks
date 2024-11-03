370e996c309440591741d843bafdd470efa5224d
Makc <142391621+MKCC1@users.noreply.github.com>
2024-11-03 17:32:01 +0300

    Delete Лабораторная работа 1 directory

--- Лабораторная работа 1/task_1.py
+++
@@ -1,9 +0,0 @@
-numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
-# TODO заменить значение пропущенного элемента средним арифметическим
-numbers_without_none = numbers[0:4] + numbers[5:]
-total = sum(numbers_without_none)
-count = (len(numbers))
-avg = total / count
-numbers[4] = avg
-#print(f'Список без пропущенного {numbers_without_none}\nсумма:{total}\nдлинна:{count}\nср.арф.:{avg}')
-print("Измененный список:", numbers)
